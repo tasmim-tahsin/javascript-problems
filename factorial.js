3ebfff6a8cba595f5d7981b6ba583f013e56f38a
Md Tasmim Al Tahsin <tahsinniyan@gmail.com>
2023-08-04 22:53:37 +0600

    while loop added

--- factorial.js
+++ factorial.js
@@ -1,3 +1,4 @@
+//for loop
 function facto(number)
 {
     let result=1;
@@ -8,4 +9,20 @@ function facto(number)
     }
     return result;
 }
+//while Loop
+function facto_while(number)
+{
+    let result=1;
+    let i=1;
+
+    while(i<=number)
+    {
+        result*=i;
+        i++;
+    }
+    return result;
+}
+
+
 console.log(facto(5));
+console.log(facto_while(3));
